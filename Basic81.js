e03456ea8abc0084207675b0364b9d851fec5c6f
DzmitryYesis <yesis1988@mail.ru>
2021-10-26 17:09:33 +0300

    created basic81

---
+++ Basic81.js
@@ -0,0 +1,9 @@
+var myDog = {
+    // Only change code below this line
+    "name": "Gretta",
+    "legs": 4,
+    "tails":1,
+    "friends":["mama","papa"]
+    
+    // Only change code above this line
+    };
