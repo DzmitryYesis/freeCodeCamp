6b29482d95a9a2a8a28160d103c190b503701d15
DzmitryYesis <yesis1988@mail.ru>
2021-10-26 18:49:09 +0300

    created basic90

---
+++ Basic90.js
@@ -0,0 +1,24 @@
+var myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }
+  ];
+  var kih={
+    "artist":"Gorhok",
+    "title":"Silly",
+    "release_year":2000,
+    "formats":[
+      "CD",
+      "BB",
+      "OOOOOOOps"
+    ]
+  };
+  myMusic.push(kih)
